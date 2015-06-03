4895aee816e23c3ffc06ea785461505d30d041cf
dantz007 <ishandmc1996@gmail.com>
2015-06-03 10:27:50 +0530

    Update, cleanup and organize

--- cm.mk
+++ cm.mk
@@ -4,6 +4,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := cm_xolo_q1100
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_GMS_CLIENTID_BASE := android-xolo
